41244e80eab1ddedc2ca7c30dd791227ba6cbe2c
Alex Yu <alexcwyu@gmail.com>
2026-04-06 23:23:02 +0800

    standardize

--- liualgotrader/common/exceptions.py
+++
@@ -1,6 +0,0 @@
-class LiuException(Exception):
-    """Base class for framework exceptions"""
-
-
-class MarketClosedToday(LiuException):
-    """Base class for framework exceptions"""
--- liualgotrader/models/ticker_snapshot.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class TickerSnapshot:
-    symbol: str
-    volume: int
-    today_change: float
